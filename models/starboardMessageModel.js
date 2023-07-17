81852eb33cd5d10c9252491ab46c9ce67106cd1b
BeauTheBeau <beauthebeau@protonmail.me>
2023-07-17 20:27:18 +0100

    Add XP and Level to userModel and refine starboardMessageModel
    This commit adds 'XP' and 'level' fields to the 'userModel' to support a new feature of user progress tracking. In addition, the 'starboardMessageModel' is updated to no longer have a 'sent' field but instead a 'starboardMessageID' field to better handle the process of tracking and storing messages that have been 'starstruck'.

--- models/starboardMessageModel.js
+++ models/starboardMessageModel.js
@@ -4,8 +4,8 @@ const starboardMessageSchema = new mongoose.Schema({
     messageID: {type: String, required: true},
     guildID: {type: String, required: true},
     channelID: {type: String, required: true},
-    starstruck: {type: Array, required: true},
-    sent: {type: Boolean, required: true, default: false}
+    starboardMessageID: {type: String, required: false},
+    starstruck: {type: Array, required: true}
 });
 
 const model = mongoose.model(`starboardMessageModel`, starboardMessageSchema);
