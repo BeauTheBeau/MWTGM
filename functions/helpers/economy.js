e0376d99deb69767193d9a8e6439b5a0314516ca
BeauTheBeau <beauthebeau@proton.me>
2023-07-22 23:46:24 +0100

    Add removeMoney function and bank parameter to addMoney
    The change was made to extend user's economy functionality.
    A new function removeMoney is added, so money can now be also removed from the user's balance.
    A new bank argument is added to both the functions for handling bank related operations.

--- functions/helpers/economy.js
+++ functions/helpers/economy.js
@@ -11,10 +11,11 @@ const userModel = require(`../../models/userModel.js`)
  * @description Adds money to a user's balance
  * @param {String} userID - The ID of the user
  * @param {Number} amount - The amount of money to add
+ * @param bank
  * @returns {Promise<void>}
  */
 
-async function addMoney (userID, amount) {
+async function addMoney (userID, amount, bank = false) {
 
   try {
     const user = await userModel.findOneAndUpdate(
@@ -33,4 +34,37 @@ async function addMoney (userID, amount) {
   }
 }
 
+/**
+ * @function removeMoney
+ * @description Removes money from a user's balance
+ * @param {String} userID - The ID of the user
+ * @param {Number} amount - The amount of money to remove
+ * @param bank
+ * @returns {Promise<void>}
+ * @throws {Error} - If the user is not found
+ * @throws {Error} - If the user does not have enough money
+ */
 
+async function removeMoney (userID, amount, bank = false
+
+    try {
+      const user = await userModel.findOneAndUpdate(
+        { userID: userID },
+        { $inc: { cash: -amount } },
+        { new: true, upsert: false, runValidators: true }
+      )
+
+      if (!user) throw new Error('User not found')
+      if (user.cash < 0) throw new Error('User does not have enough money')
+
+      return user;
+
+    } catch (e) {
+      console.error(e.stack)
+      throw e
+    }
+}
+
+/**
+ *
+ */
